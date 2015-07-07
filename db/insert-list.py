307d18aba530770a2491141847fbf0e077698e48
aidanb <aidanb@gmail.com>
2015-07-07 21:06:16 +0200

    Fixed up directories

--- db/insert-list.py
+++
@@ -1,9 +0,0 @@
-from db import insertNewSite, getDateCrawled
-
-
-# utility script to insert starter list from a text file.
-
-with open('list.txt') as f:
-    for line in f:
-        url = line.strip().translate(None, '/')
-        print url
